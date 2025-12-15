a92681671b3b382f736c830e84471e6e882307b9
User Name <user@example.com>
2025-12-15 21:54:17 +0300

    Remove unnecessary files from repository

--- tinkoff_business.py
+++
@@ -1,7 +0,0 @@
-class TinkoffBusinessAPI:
-    def __init__(self, *args, **kwargs):
-        pass
-    
-    def create_invoice(self, *args, **kwargs):
-        # Mock implementation
-        return {'terminal_key': 'mock_terminal_key', 'order_id': 'mock_order_id'}
